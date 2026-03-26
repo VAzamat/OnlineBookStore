913f1481f59a86bd0efa556bfe08c72b468980be
Valeev Azamat <vazamat@gmail.com>
2026-03-27 01:29:47 +0300

    realize postgres db connection

--- config/settings.py
+++ config/settings.py
@@ -80,9 +80,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME":     os.environ.get('POSTGRES_DATABASE_NAME'),
+        "USER":     os.environ.get('POSTGRES_DATABASE_USER'),
+        "PASSWORD": os.environ.get('POSTGRES_DATABASE_USER_PASSWORD'),
+        "HOST":     os.environ.get('POSTGRES_DATABASE_HOST'),
+        "PORT":     os.environ.get('POSTGRES_DATABASE_PORT'),
     }
 }
 
