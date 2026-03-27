763431bfa3524f8041e29c6dd08706691d73f509
Valeev Azamat <vazamat@gmail.com>
2026-03-27 05:04:03 +0300

    try to solve the HEADER invisibility issue

--- config/settings.py
+++ config/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.109', '127.0.0.1', 'localhost']
 
 # Application definition
 
